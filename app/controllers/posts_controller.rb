9eb6f0ecac4a3e10d06461755ccaad8270102b51
saguiguilid <tbki.jea@gmail.com>
2020-10-09 21:42:35 +0800

    Generate scaffold for Post model

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,19 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @posts = Post.all.order(created_at: :desc)
+    @post = Post.new
+  end
+
+  def create
+    Post.create(post_params)
+    redirect_to posts_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:body)
+  end
+end
